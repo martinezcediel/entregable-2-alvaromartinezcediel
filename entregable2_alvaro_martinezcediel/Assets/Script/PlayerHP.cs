3ae0a46c7f140fcf84e976bed1073ed17bebcab3
Álvaro Martínez-Cediel Algora <alvaro.martinez.algora@gmail.com>
2021-10-19 19:23:53 +0200

    entregable2

---
+++ entregable2_alvaro_martinezcediel/Assets/Script/PlayerHP.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHP : MonoBehaviour
+{
+    [SerializeField] private bool isGameOver;
+    [SerializeField] private int HP = 100;
+    public int damage = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HP -= damage;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGameOver == false)
+        {
+            if (HP <= 0)
+            {
+                Debug.Log(message: "Te has quedado sin vida... GAMEOVER");
+            }
+            else if (HP < 20)
+            {
+                Debug.Log(message: "!Tienes poca vida¡");
+            }
+            else
+            {
+                Debug.Log(message: $"Vas bien de vida: te quedan {HP} puntos de vida");
+            }
+        }
+        else
+        {
+            Debug.Log(message: "GAME OVER");
+        }
+    }
+}
